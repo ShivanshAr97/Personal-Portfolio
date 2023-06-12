69e6ea989355850fb94ce05d52dd28df2220f105
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-06-12 23:35:43 +0530

    firstCommit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import About from './components/About'
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+  <>
+  <About/>
+      {/* <Routes>
+        <Route path='/about' element={<About />} />
+      </Routes> */}
+  </>
+  )
+}
+
+export default App;
